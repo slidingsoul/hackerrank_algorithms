47bc017ad5d50c688a35b78961fce11680629e78
Daniel <141699797+slidingsoul@users.noreply.github.com>
2026-03-13 09:47:06 +0700

    new prob

---
+++ easy/circular_array_rotation.py
@@ -0,0 +1,16 @@
+def circularArrayRotation(a, k, queries):
+  new_arr = a[:]
+  for n in range(len(a)):
+    a_num = a[n]
+    new_pos = (n + k) % len(a)
+    new_arr[new_pos] = a_num
+  result = []
+  for q in queries:
+    result.append(new_arr[q])
+  return result
+
+a = [3, 4, 5]
+k = 2
+queries = [1, 2]
+
+print(circularArrayRotation(a, k, queries))
